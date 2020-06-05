0ab7e0d7317ad07c5764c5c44c8c20055d24229f
lchrennew <lchrennew@126.com>
2020-06-05 10:19:50 +0800

    Update step_implementation.js

--- gauge-demo/tests/step_implementation.js
+++ gauge-demo/tests/step_implementation.js
@@ -3,20 +3,14 @@
 const fetch = require('node-fetch');
 const assert = require("assert");
 
-step("Open Url <url>", async (url) => {
-	await fetch(url)
-    .then(res => res.json())
-    .then(json => {
-		console.log(json);
-		assert.equal(json.pwd,'admin',"用户密码不正确");
-	});
+step("Open Url <url>", async url => {
+	const response = await fetch(url)
+	const {pwd} = await response.json()
+	assert.equal(pwd,'admin',"用户密码不正确");
 });
 
-step("Accert URL <url> Name <name>",async(url,name)=>{
-	await fetch(url)
-    .then(res => res.json())
-    .then(json =>{
-		console.log(json);
-		assert.equal(json.name,name,"用户名不正确");
-	});
-});
+step("Accert URL <url> Name <name>",async (url,name)=>{
+	const response = await fetch(url)
+	const result = await response.json()
+	assert.equal(result.name,name,"用户名不正确");
+})
