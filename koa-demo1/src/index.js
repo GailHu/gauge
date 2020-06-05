04627021d6e3001931961695ef76c2421536b74a
lchrennew <lchrennew@126.com>
2020-06-05 09:42:32 +0800

    Update index.js

--- koa-demo1/src/index.js
+++ koa-demo1/src/index.js
@@ -4,17 +4,16 @@ const Router = require('koa-router');
 const app = new Koa();
 const router = new Router();
 
-app.use(router.routes(),router.allowedMethods());
-
-//定义路由
-router.get('/getJson', function (ctx, next) {
-	returnJson(ctx);
-});
 
 //返回json
 let returnJson =((ctx)=>{
 	ctx.body = '{"name":"admin","pwd":"admin"}';  
 });
+
+//定义路由
+router.get('/getJson', returnJson);
+
+app.use(router.routes(),router.allowedMethods());
 
 app.listen(3000, () => {
   console.log('http://127.0.0.1:3000');
